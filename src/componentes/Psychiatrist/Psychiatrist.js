73d18ca1894f4998a07238f8989a29168ac10ade
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-09-25 20:28:38 +0600

    ready for deploy

--- src/componentes/Psychiatrist/Psychiatrist.js
+++ src/componentes/Psychiatrist/Psychiatrist.js
@@ -24,7 +24,7 @@ function Psychiatrist() {
 
                     >
                         <div class="flex items-center m-10 border border-gray-200 rounded shadow p-3 flex-col md:flex-row">
-                            <img src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg" alt="" width="130" class="rounded mr-4" />
+                            <img src={doctor.img} alt="" width="130" class="rounded mr-4" />
                             <div class="text-gray-700 overflow-hidden ">
                                 <h2 class="text-gray-900 font-bold text-2xl">{doctor.name}</h2>
                                 <div class="text-sm">{doctor.email}</div>
@@ -32,10 +32,10 @@ function Psychiatrist() {
                                     <p className="text-lg font-bold">Specialities: <span className="font-thin">{doctor.specialities}</span>
                                     </p>
 
-                                    <p className="text-lg font-bold"> Education: <span className="font-thin">{doctor.experiance}</span>
+                                    <p className="text-lg font-bold"> Education: <span className="font-thin">{doctor.education}</span>
                                     </p>
 
-                                    <p className="text-lg font-bold"> experiance: <span className="font-thin"> {doctor.education}</span>
+                                    <p className="text-lg font-bold"> experiance: <span className="font-thin"> {doctor.experiance}</span>
                                     </p>
 
                                     <button className="bg-[#9BD58B] px-10 py-2 text-xl rounded-full hover:text-white hover:bg-[#4B7B3F] font-bold my-5">
